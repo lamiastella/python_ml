a4af1acd0d2c643dcfa05432a50c7d4b5b0ccf0b
mona <jalal@wisc.edu>
2016-03-01 21:46:28 -0600

    all

---
+++ ds.py
@@ -0,0 +1,9 @@
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+X=np.array([[-1,1], [-2,-1],[-3,-2],[1,1],[2,1],[3,2]])
+Y=np.array([1,1,1,2,2,2])
+clf=GaussianNB()
+clf.fit(X,Y)
+GaussianNB()
+print(clf.predict([[-0.8,-1]]))
+
